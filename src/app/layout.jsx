75a4dc923c61539e80692fe2f9f290c9d8f035da
Qaerus <97757426+Qaerus@users.noreply.github.com>
2025-06-13 12:10:52 -0400

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,17 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
-  title: 'AI Outreach Landing Page',
-  description: 'Get more leads using AI + cold email',
+  title: "AI Outreach Landing Page",
+  description: "Get more leads using AI + cold email",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
